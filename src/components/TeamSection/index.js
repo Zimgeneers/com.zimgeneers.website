3d84abbf619c5e84cdaa78a8fbad223b90d9daf8
Jiros <jamie@munro.consulting>
2022-08-22 14:11:49 +0100

    LinkedIn links

--- src/components/TeamSection/index.js
+++ src/components/TeamSection/index.js
@@ -7,7 +7,7 @@ function TeamSection() {
        <span class="anchor" id="Team"></span>
        <section class="container">
        <div class="card">
-            <a href="/">
+            <a href="https://www.linkedin.com/in/angus-currie-022571241/" target="_blank">
                 <div class="card-image gus"></div>
                 <h2>Angus Currie</h2>
                 <p>A veterinarian based in Perth, Australia with over ten years of dedicated experience in the pet travel industry.</p>
@@ -15,7 +15,7 @@ function TeamSection() {
             </a>
         </div>
         <div class="card">
-            <a href="/">
+            <a href="https://www.linkedin.com/in/jirosmunro/" target="_blank">
                 <div class="card-image jamie"></div>
                 <h2>Jamie Munro</h2>
                 <p>An IT consultant based in London, United Kingdom with over 25 years of broad industry experience including software development and IoT.</p>
@@ -23,7 +23,7 @@ function TeamSection() {
             </a>
         </div>
         <div class="card">
-            <a href="/">
+            <a href="https://www.linkedin.com/company/zimgeneers/" target="_blank">
                 <div class="card-image zim"></div>
                 <h2>Zimgeneers</h2>
                 <p>Established in 2021, Zimgeneers is a company that is focused on keepings pets (and their owners) happy on their travels. Gus and Jamie have known each other since they went to school together. </p>
