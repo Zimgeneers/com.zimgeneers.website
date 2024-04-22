b0e467556fb4193689dd733e74ec1d76b5f8c594
Jiros <jamie@munro.consulting>
2024-04-22 11:30:39 +0200

    add address

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,8 @@ const Footer = () => {
                         <SocialIcon />
                             ZIMGENEERS 
                     </SocialLogo> */}
-                    <WebsiteRights>ACN: 653 169 923<br />zimgeneers © {new Date().getFullYear()} </WebsiteRights>
+                    <WebsiteRights>ACN: 653 169 923<br />Registered office:
+Heaney Business Group, Unit 7 , 12 Belgravia Terrace , Rockingham WA 6168<br />zimgeneers t/a ziso.io © {new Date().getFullYear()}</WebsiteRights>
                     <SocialIcons>
                         <SocialIconLink href="https://www.linkedin.com/company/zimgeneers/" target="_blank" aria-label="LinkedIn">
                             <FaLinkedin />
