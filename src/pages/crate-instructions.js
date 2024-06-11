f7dda8e26b3841f72eefd3e2b94ef35f0678a16a
Jiros <jamie@munro.consulting>
2024-06-11 20:18:27 +0200

    Add Crate Instruction page

---
+++ src/pages/crate-instructions.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+
+const CratePage = () => (
+  <Layout>
+    <Seo title="Home" />
+      Watch this space for instructions...
+  </Layout>
+)
+
+export default CratePage
